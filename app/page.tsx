367eba56b4a85434eb3ebe91ec934933424b157a
cadesix <cadelaranang@gmail.com>
2025-12-27 19:09:12 -0800

    metadata

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,14 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center">
+    <div className="flex min-h-screen flex-col items-center justify-center bg-white">
       <div className="flex flex-col items-center gap-8">
-        <div className="relative h-64 w-64 bg-gray-200 rounded-lg">
+        <div className="relative h-64 w-64">
           <Image
             src="/img/gastank.png"
             alt="Gas"
             fill
-            className="object-cover rounded-lg"
+            className="object-cover"
           />
         </div>
       </div>
